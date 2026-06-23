64b88415eca26f9a0da89e1e2068cc66ed5dd2f8
jupojuji <160387850+justinpoonjj@users.noreply.github.com>
2026-06-23 17:01:45 +0800

    Done with Day-2 of python

---
+++ my_solutions/Day_2/Day_2.py
@@ -0,0 +1,39 @@
+"""
+Built in functions: 
+
+Built in functions that are globally available for use
+    - Can make use of built-in funcitons without importing or configuring
+
+Variables: 
+
+Store data in computer memory. 
+
+Mnenmonic variable (recommended): A variable name that can be easily remembered and associated
+    - A variable refers to a memory address which data is stored
+    - variable can have short name but a more descriptive name is recommended
+
+Variable name rules: 
+    - Start with a letter or underscore char
+    - Cannot start with a number
+    - Can only contain alpha-numeric characters and underscores
+    - Names are case-sensitive
+
+Standard naming style: 
+    - snake_case variable naming convention
+        - Use underscore character after each word for a variable containing more than one word
+        eg. first_name, last_name
+
+Variable declaration: assign a data type to a variable using "="
+
+Data Types: 
+
+Understand different data types very well
+
+Check Data Types and Casting: type()
+Casting - Converting one data type to another: int(), str(), float()
+
+Numbers: 
+    - Integer
+    - Floating
+    - Complex
+"""
---
+++ my_solutions/Day_2/variables.py
@@ -0,0 +1,68 @@
+# Day 2: 30 Days of python programming
+
+#----------------Execise 1------------------
+first_name = "John"
+last_name = "Doe"
+full_name = "John Doe"
+country = "Singapore"
+city = "Singapore"
+age = 23
+year = 2003
+is_married = False
+is_true = True
+is_light_on = True
+
+x = 1; y = 2; z = 3
+
+#-------------Exercise 2---------------------
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(first_name))
+print(type(is_true))
+print(type(is_light_on))
+print(type(x), type(y), type(z))
+
+print(len(first_name))
+
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+
+diff = num_one - num_two
+
+product = num_one * num_two
+
+divison = num_one / num_two
+
+remainder = num_one % num_two
+
+exp = num_one ** num_two
+
+floor_division = num_one // num_two
+
+radius = 30
+PI = 3.14
+area_of_circle = PI * radius**2
+circum_of_circle = 2 * PI * radius
+print(area_of_circle)
+print(circum_of_circle)
+
+radius = int(input("What is the radius of circle: "))
+area_of_circle = PI * radius**2
+print(area_of_circle)
+
+first_name = input("First_name: ")
+last_name = input("Last name: ")
+country = input("Country: ")
+age = input("Age: ")
+
+help('keywords')
